20b25431c760b2b9c3a8814dc6c6914f3e9f0723
hrsma2i <hrs.ma2i@gmail.com>
2017-11-30 16:47:33 +0900

    Divide SVD_SGD into SVD_SGD, MF_base Because share the common part with SVD_ALS.

--- recommendation.py
+++ recommendation.py
@@ -1,309 +1,213 @@
+#!/usr/bin/env python
+
 
 # coding: utf-8
 
 # In[ ]:
 
-
-from copy import deepcopy
 import os
+import argparse
 import codecs
-from itertools import product
 
-from IPython.display import display
 from tqdm import tqdm, trange
+import joblib
+from IPython.display import display, Image
 import numpy as np
-from numpy.linalg import inv, solve
+from numpy.linalg import solve
 import pandas as pd
+pd.set_option('display.max_colwidth', 1000)
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 
 # In[ ]:
 
-
-def rms(arr):
-    return np.sqrt(np.mean((arr**2).ravel()))
-
-
-# In[ ]:
-
-
-def SVD(R, n_epochs=20, K=100, lr=0.05, reg=0.02):
+class MF_base:
+    def predict(self, users, items):
+        R_p = self.get_R_p
+        pred = R_p[users, items]
+        if len(users) == 1:
+            return pred[0]
+        return pred
     
-    # U: the number of users
-    # I: the number of items
-    U, I = R.shape
-    # P: user factor matrix
-    P = np.random.rand(K, U)
-    # Q: item factor matrix
-    Q = np.random.rand(K, I)
-    
-    # only use rated elements of R to compute loss
-    mask = np.ones(R.shape)
-    mask[R == 0] = 0
-    
-    # learning
-    for epoch in trange(n_epochs, desc='epoch'):
-        pbar = tqdm(product(range(U), range(I)),
-                 total=U*I, desc='(u, i)')
-        for u, i in pbar:
-            # only update rated elements
-            Rui = R[u,i]
-            if Rui > 0:
-                # compute error
-                eui = Rui - P[:,u].T.dot(Q[:,i])
-                for k in range(K):
-                    Pku = P[k,u]
-                    Qki = Q[k,i]
-                    P[k,u] += lr * (2 * eui * Qki - reg * Pku)
-                    Q[k,i] += lr * (2 * eui * Pku - reg * Qki)
-                    
-        # compute & print loss
-        E = R - (P.T.dot(Q))*mask
-        loss = rms(E) + reg/2.0*(rms(P)+rms(Q))
-        tqdm.write('epoch {}: {}'.format(epoch, loss))
-        
-        if loss < 0.001:
-            break
-            
-    return P, Q
-
-
-# In[ ]:
-
-
-def SVD_batch(R, n_epochs=100, K=100, lr=0.005, reg=0.02,
-             threshold=0.01):
-    # U: the number of users
-    # I: the number of items
-    U, I = R.shape
-    # P: user factor matrix
-    P = np.random.rand(K, U)
-    # Q: item factor matrix
-    Q = np.random.rand(K, I)
-    bU = np.zeros(U)
-    bI = np.zeros(I)
-    
-    # only use rated elements of R to compute loss
-    mask = np.ones(R.shape)
-    mask[R == 0] = 0
-    
-    pre_loss = None
-    chance = 2
-    
-    vld = Validater()
-    # learning
-    for epoch in trange(n_epochs, desc='epoch'):
-        for u in trange(U):
-            Pu = P[:,u] # (K, 1)
-            # (1, I)
-            Eu = (R[u,:] -Pu.T.dot(Q)
-                  -bU[u] -bI)*mask[u,:] 
-            # (K, 1)
-            P[:,u] += lr*(np.mean(Eu*Q, axis=1)
-                          - reg*Pu)
-            bI += lr*(Eu.ravel() - reg*bI)
-            
-        for i in trange(I):
-            Qi = Q[:,i] # (K, 1)
-            # (U, 1)
-            Ei = (R[:,i] -P.T.dot(Qi)
-                 -bU -bI[i])*mask[:,i]
-            # (K, 1)
-            Q[:,i] += lr*(np.mean(Ei.T*P, axis=1)
-                          - reg*Qi)
-            bU += lr*(Ei.ravel() - reg*bU)
-                    
-        # compute & print loss
-        R_p = P.T.dot(Q) + bU.reshape(-1,1) + bI.reshape(1,-1)
-        loss = np.sqrt(mean_squared_error(R*mask, R_p*mask))
-        tqdm.write('epoch {}'.format(epoch))
-        tqdm.write(' train loss:{}'.format(loss))
-        tqdm.write(' val loss:'+str(vld.validate(R_p)))
-        
-        if loss < threshold:
-            break
-        
-        pre_loss = deepcopy(loss)
-        
-    return P, Q, bU, bI
-
-
-# In[ ]:
-
-
-def SVD_ALS(R, n_epochs=70, f=1000,  reg=0.1):
-    # m: the number of users
-    # n: the number of items
-    m, n = R.shape
-    # user factors
-    X = np.random.rand(m, f)
-    # item factors
-    Y = np.random.rand(n, f)
-    
-    mask = np.ones(R.shape)
-    mask[R == 0] = 0
-    
-    vld = Validater()
-    
-    for epoch in trange(n_epochs):
-        regI = reg * np.eye(f)
-        
-        YtY = Y.T.dot(Y)
-        for u in trange(m):
-            X[u] = solve((YtY + regI), 
-                        R[u, :].dot(Y))
-
-        XtX = X.T.dot(X)
-        for i in trange(n):
-            Y[i] = solve((XtX + regI), 
-                        R[:, i].dot(X))
-            
-        # compute & print loss
+    def get_R_p(self):
+        X = self.X
+        Y = self.Y
         R_p = X.dot(Y.T)
-        loss = np.sqrt(mean_squared_error(R[mask==1], R_p[mask==1]))
-        print('sgd')
-        tqdm.write('epoch {}'.format(epoch))
-        tqdm.write(' train loss: {}'.format(loss))
-        tqdm.write('   val loss: '+str(vld.validate(R_p)['RMSE']))
+        return R_p
         
-    return X, Y
-
-
-# In[ ]:
-
-
-def SVDpp(R, n_epochs=70, f=100, alpha=40, reg=50):
-    # m: the number of users
-    # n: the number of items
-    m, n = R.shape
-    # user factors
-    X = np.random.rand(m, f)
-    # item factors
-    Y = np.random.rand(n, f)
-    # preference
-    P = np.ones(R.shape)
-    P[R==0] = 0
-    # confidence
-    C = 1 + alpha*R
+    def test(self, df_test):
+        users = df_test['user_id']
+        items = df_test['item_id']
+        obs = df_test['rating']
+        pred = self.predict(users, items)
+        evaluation = np.sqrt(mean_squared_error(obs, pred))
+        return evaluation
     
-    for epoch in trange(n_epochs):
-        for u in trange(m):
-            Cu = np.diag(C[u])
-            X[u] = inv(Y.T.dot(Cu).dot(Y)+reg).dot(
-                         Y.T).dot(
-                         Cu).dot(
-                         P[u])
-
-        for i in trange(n):
-            Ci = np.diag(C[:,i])
-            Y[i] = inv(X.T.dot(Ci).dot(X)+reg).dot(
-                         X.T).dot(
-                         Ci).dot(
-                         P[:,i])
-        R = X.dot(Y.T)
-        tqdm.write(str(pd.Series(R.ravel()).describe()))
-        
-    return X, Y
-
-
-# In[ ]:
-
-
-class Validater:
-
-    # load test data
-    test_file = './data/u1.test'
-    df_test = pd.read_csv(test_file, delimiter='\t', header=None)
-    df_test.columns = ['user_id', 'item_id', 'rating', 'timestamp']
+    def get_similarities(self):
+        """
+        Compute similartiteis matrix (m, m), using cosine
+        """
+        Y = self.Y
+        sim = model.Y.dot(Y.T)
+        norms = np.sqrt(np.diagonal(sim)).reshape(-1,1)
+        return sim / norms/ norms.T
     
-    def validate(self, R_p):
-        df_test = self.df_test
+    def update(self):
+        pass
         
-        # get observations and predictions
-        obs = df_test['rating'].values
-        pred = R_p[df_test.values[:,0]-1, df_test.values[:,1]-1]
+    def fit(self, R ,df_val=None, out=None):
+        """
+        R (array): rating matrix
+        df_test (DataFrame):
+            - columns (user, item rating)
+        """
+        self.R = R
+        
+        f = self.f
+        n_epochs = self.n_epochs
+        
+        m, n = R.shape
+        X = np.random.rand(m, f) # user factors
+        Y = np.random.rand(n, f) # item factors
+        self.X = X
+        self.Y = Y
 
-        # compute evaluation metrics
-        results = {
-            "MAE":mean_absolute_error(obs, pred),
-            "RMSE":np.sqrt(mean_squared_error(obs, pred))
-        }   
-
-        return results
-
-
-# In[ ]:
-
-
-def main(fold_id=1):
-    # load dataset
-    data_file = './data/u{}.base'.format(fold_id)
-    df_data = pd.read_csv(data_file, delimiter='\t', header=None)
-    df_data.columns = ['user_id', 'item_id', 'rating', 'timestamp']
-    n_users = df_data.max()['user_id']
-    n_items = df_data.max()['item_id']
-
-    # change shape into user-item matrix
-    ratings = df_data.pivot(index='user_id', columns='item_id',
-                            values='rating').fillna(0)
-    # fill the lack of no-rated item_id
-    for item in range(n_items):
-        item += 1
-        if item not in ratings.columns:
-            ratings.loc[:, item] = 0
+        for epoch in trange(n_epochs):
+            self.update()
+                
+            # compute train loss
+            R_p = self.get_R_p()
+            loss = np.sqrt(mean_squared_error(R[R!=0], R_p[R!=0]))
+            tqdm.write('epoch {:03d}:'.format(epoch))
+            tqdm.write('train loss: {}'.format(loss))
             
-    # learning
-    R = ratings.values
-    P, Q, bU, bI = SVD_batch(R, lr=0.3, n_epochs=10000)
-    
-    # save matrices
-    np.savetxt('others/P.csv', P)
-    np.savetxt('others/Q.csv', Q)
-    np.savetxt('others/bU.csv', bU)
-    np.savetxt('others/bI.csv', bI)
-
-
-# In[ ]:
-
-
-def mainpp(fold_id=1):
-    # load dataset
-    data_file = './data/u{}.base'.format(fold_id)
-    df_data = pd.read_csv(data_file, delimiter='\t', header=None)
-    df_data.columns = ['user_id', 'item_id', 'rating', 'timestamp']
-    n_users = df_data.max()['user_id']
-    n_items = df_data.max()['item_id']
-
-    # change shape into user-item matrix
-    ratings = df_data.pivot(index='user_id', columns='item_id',
-                            values='rating').fillna(0)
-    # fill the lack of no-rated item_id
-    for item in range(n_items):
-        item += 1
-        if item not in ratings.columns:
-            ratings.loc[:, item] = 0
+            # compute validation loss
+            if df_val is not None:
+                val_loss = self.test(df_val)
+                tqdm.write('  val loss: {}'.format(val_loss))
             
-    # learning
-    R = ratings.values
-    X, Y = SVD_ALS(R)
-    
-    # save matrices
-    np.savetxt('others/X.csv', X)
-    np.savetxt('others/Y.csv', Y)
+        
+        # save parameters
+        params = {
+            'R': R,
+            'X': X,
+            'Y': Y,
+        }
+        self.params = params
+        if out is not None:
+            if not os.path.exists(out):
+                os.makedirs(out)
+            
+            params_file = os.path.join(out, 'parameters.pkl')
+            joblib.dump(params, params_file, compress=True)
+            
+    def load_params(self, params_file):
+        params = joblib.load(params_file)
+        for k, v in params.items():
+            exec('self.{} = v'.format(k))
 
 
 # In[ ]:
 
+class SVD_SGD(MF_base):
+    def __init__(self, f=100, n_epochs=20, lr=0.005, reg=0.02, biased=False):
+        self.f = f
+        self.n_epochs = n_epochs
+        self.lr = lr
+        self.reg = reg
+        self.biased = biased
+
+    def update(self):
+        lr = self.lr
+        reg = self.reg
+        R = self.R
+        X = self.X
+        Y = self.Y
+        r_ui_notzero = zip(*np.where(R!=0))
+        for u, i in r_ui_notzero:
+            e = R[u,i] - X[u].dot(Y[i].T)
+            X[u] += lr * (e*Y[i] - reg*X[u])
+            Y[i] += lr * (e*X[u] - reg*Y[u])
+
+
+# In[ ]:
+
+class SVD_ALS(MF_base):
+    def __init__(self, f=100, n_epochs=20, reg=0.02, biased=False):
+        self.f = f
+        self.n_epochs = n_epochs
+        self.reg = reg
+        self.biased = biased
+
+    def fit(self, R ,df_val=None):
+        """
+        R (array): rating matrix
+        df_test (DataFrame):
+            - columns (user, item rating)
+        """
+        
+        f = self.f
+        n_epochs = self.n_epochs
+        reg = self.reg
+        
+        m, n = R.shape
+        X = np.random.rand(m, f) # user factors
+        Y = np.random.rand(n, f) # item factors
+
+        for epoch in trange(n_epochs):
+            regI = reg * np.eye(f)
+
+            YtY = Y.T.dot(Y)
+            for u in trange(m):
+                X[u] = solve((YtY + regI), 
+                            R[u, :].dot(Y))
+
+            XtX = X.T.dot(X)
+            for i in trange(n):
+                Y[i] = solve((XtX + regI), 
+                            R[:, i].dot(X))
+            
+            R_p = X.dot(Y.T)
+            loss = np.sqrt(mean_squared_error(R[R!=0], R_p[R!=0]))
+            tqdm.write('epoch {:03d}:'.format(epoch))
+            tqdm.write('train loss: {}'.format(loss))
+            
+            self.R_p = R_p
+            if df_val is not None:
+                val_loss = self.test(df_val)
+                tqdm.write('  val loss: {}'.format(val_loss))
+
+
+# In[ ]:
 
 if __name__=='__main__':
-    fold_id = 1
+    # get parameters from terminal
+    parser = argparse.ArgumentParser(description='Matrix Factorization')
+    parser.add_argument('-f', '--fold_id', type=int, default=1,
+                       help='id of fold to validate')
+    parser.add_argument('-e', '--n_epochs', type=int, default=10,
+                       help='the number of epochs to train')
+    parser.add_argument('-o', '--out', type=str, default='results',
+                       help='the path where the training results will\
+                       be')
+    args = parser.parse_args()
+    fold_id = args.fold_id
+    n_epochs = args.n_epochs
+    out = args.out
+    
+    # load train data
     data_file = './data/u{}.base'.format(fold_id)
     df_data = pd.read_csv(data_file, delimiter='\t', header=None)
     df_data.columns = ['user_id', 'item_id', 'rating', 'timestamp']
     n_users = df_data.max()['user_id']
     n_items = df_data.max()['item_id']
+    
+    # load validation data
+    val_file = './data/u{}.test'.format(fold_id)
+    df_val = pd.read_csv(val_file, delimiter='\t', header=None)
+    df_val.columns = ['user_id', 'item_id', 'rating', 'timestamp']
 
-    # change shape into user-item matrix
+    # change the shape of train data into user-item matrix
     ratings = df_data.pivot(index='user_id', columns='item_id',
                             values='rating').fillna(0)
     # fill the lack of no-rated item_id
@@ -313,24 +217,50 @@ if __name__=='__main__':
             ratings.loc[:, item] = 0
             
     # learning
+    model = SVD_SGD(n_epochs=n_epochs)
     R = ratings.values
-    X, Y = SVD_ALS(R)
+    model.fit(R, df_val, out=out)
+
+
+# In[ ]:
+
+def recommend_topk(R, u, k=None, mask=None):
+    with codecs.open('data/u.item', 'r', 'utf-8', 'ignore') as f:
+        df_items = pd.read_csv(f, delimiter='|', header=None)
     
-    # save matrices
-    np.savetxt('others/X.csv', X)
-    np.savetxt('others/Y.csv', Y)
+    rec = pd.DataFrame({
+        'rating':R[u],
+        'title':df_items[1],
+    })
+    
+    if mask is None:
+        mask = (R[u]!=0)
+    rec = rec[mask]
+    
+    rec = rec.sort_values('rating', ascending=False)
+    
+    return rec[:k]
+
+
+# In[ ]:
+
+model = SVD_SGD()
+model.load_params('results/test/parameters.pkl')
+
+#recommend_topk(R=model.get_R_p(), u=0)
+user = 0
+rated = recommend_topk(R=model.R, u=user, k=10)
+mask_unrated = (model.R[user]==0)
+unrated = recommend_topk(R=model.get_R_p(), u=user, k=10, mask=mask_unrated)
+rated
+
+
+# In[ ]:
+
+unrated
 
 
 # In[ ]:
 
 
-if __name__=='__main__':
-    test_file = './data/u1.test'
-    df_test = pd.read_csv(test_file, delimiter='\t', header=None)
-    df_test.columns = ['user_id', 'item_id', 'rating', 'timestamp']
-
-    X = np.loadtxt('others/X.csv')
-    Y = np.loadtxt('others/Y.csv')
-    R_p = X.dot(Y.T)
-    print(pd.Series(R_p.ravel()).describe())
 
